f71024c270b0b94a3f3f7bf39e123f113263a75f
Robin Joseph <robin@lob.com>
2016-03-13 18:35:15 -0700

    chore(prod): add production config variables

--- config/production.js
+++ config/production.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports = {
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_USER: process.env.DB_USER,
   DRAIN_TIMEOUT: 60000,
   PORT: 8647,
   SALT_ROUNDS: 10
