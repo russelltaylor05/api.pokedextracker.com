890acb1e3a7060ca3781178f37a0e2de09220907
Robin Joseph <robin@lob.com>
2016-03-19 15:45:56 -0700

    feat(auth): add auth service

---
+++ test/plugins/services/auth.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const Config = require('../../../config');
+
+describe('auth service plugin', () => {
+
+  let server;
+
+  beforeEach(() => {
+    server = new Hapi.Server();
+    server.connection({ port: 80 });
+
+    server.register([
+      require('inject-then'),
+      require('../../../src/plugins/services/auth')
+    ], () => {})
+
+    server.route([{
+      method: 'GET',
+      path: '/public',
+      config: {
+        handler: (request, reply) => reply({})
+      }
+    }, {
+      method: 'GET',
+      path: '/private',
+      config: {
+        auth: 'token',
+        handler: (request, reply) => reply(request.auth.credentials)
+      }
+    }]);
+  });
+
+  it('does not do anything for public routes', () => {
+    return server.injectThen({
+      method: 'GET',
+      url: '/public'
+    })
+    .then((res) => {
+      expect(res.statusCode).to.eql(200);
+    });
+  });
+
+  it('returns a 401 if no authentication is provided for private routes', () => {
+    return server.injectThen({
+      method: 'GET',
+      url: '/private'
+    })
+    .then((res) => {
+      expect(res.statusCode).to.eql(401);
+    });
+  });
+
+  it('returns a 401 if invalid authentication is provided for private routes', () => {
+    return server.injectThen({
+      method: 'GET',
+      url: '/private',
+      headers: {
+        authorization: `Bearer ${Config.JWT_TEST.substr(0, Config.JWT_TEST.length - 1)}`
+      }
+    })
+    .then((res) => {
+      expect(res.statusCode).to.eql(401);
+    });
+  });
+
+  it('goes through if valid authentication is provided for private routes', () => {
+    return server.injectThen({
+      method: 'GET',
+      url: '/private',
+      headers: {
+        authorization: `Bearer ${Config.JWT_TEST}`
+      }
+    })
+    .then((res) => {
+      expect(res.statusCode).to.eql(200);
+      expect(res.result).to.contain.all.keys(['id', 'username', 'friend_code']);
+    });
+  });
+
+});
