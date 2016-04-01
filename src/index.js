f1e10e14ed912d52f1c709979f653ce5d0340007
Robin Joseph <robin@lob.com>
2016-03-31 22:25:00 -0700

    fix(new-relic): make sure require('newrelic') is first

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
+require('newrelic');
+
 const Util = require('util');
 
 const Config = require('../config');
 const API    = require('./server');
-
-require('newrelic');
 
 API.start(() => Util.log(`Server started on port: ${Config.PORT}`));
