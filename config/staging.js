fee851b829276b60506db45fa3dd371b6d876a97
Robin Joseph <robin@lob.com>
2016-04-26 23:54:15 -0700

    feat(config): add staging env

--- config/staging.js
+++ config/staging.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   DB_DEBUG: process.env.DB_DEBUG,
-  DRAIN_TIMEOUT: 5000,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_USER: process.env.DB_USER,
+  DRAIN_TIMEOUT: 60000,
+  JWT_SECRET: process.env.JWT_SECRET,
   PORT: 8647,
   SALT_ROUNDS: 10
 };
